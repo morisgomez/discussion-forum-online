b64ffa23f8b0e7131a2fa86a7a160b3ace07b771
Morís Gomez <83724852+morisgomez@users.noreply.github.com>
2023-11-19 18:10:05 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
+
+//index.js is in charge of rendering App component to browser. 
