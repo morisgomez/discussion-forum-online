7e363fce2b6bc9a68dd09d451c55687836fea94d
Morís Gomez <83724852+morisgomez@users.noreply.github.com>
2023-11-19 18:15:19 -0800

    Update Card.js

--- Card.js
+++ Card.js
@@ -16,6 +16,6 @@ function Card(props) {
 export default Card;
 //Explaining lines 7 - 14:
 //card component takes in the prop "commentObject", which is 1 of the 3 objects,coming from App component.
-//the Header instance gets passed 2 props 1) profileImg and 2) username. the value of these are the the profileImg and username value from the commentObject prop.
-//the Body instance gets 1 prop titles comment and its value is the actual comment.
-//props.commentObject.comment === we are accessing an object with 3 key-value pairs from the props object (object from another object -- can get confusing).
+//the Header instance gets passed 2 props 1) profileImg and 2) username. the value of these are the profileImg and username value from the commentObject prop.
+//the Body instance gets 1 prop titled comment and its value is the actual comment.
+//props.commentObject.comment means we are accessing an object with 3 key-value pairs from the props object (object from another object -- this can get confusing).
